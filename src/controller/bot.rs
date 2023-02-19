e84fa177b0476dd597b6d1f565a01ee0ce5c01de
yugogamer <57806524+yugogamer@users.noreply.github.com>
2023-02-19 22:39:24 +0100

    fix api roll

--- src/controller/bot.rs
+++ src/controller/bot.rs
@@ -4,9 +4,9 @@ use actix_web::{
     HttpResponse,
 };
 
-use crate::services::models::auth::LightUser;
 use crate::services::models::character::CharacterStore;
 use crate::services::models::roll::roll_initiative;
+use crate::services::models::{auth::LightUser, groupes::GroupeStore};
 use crate::{services::database::Database, utils::errors::AppErrors};
 
 #[get("/roll/{groupe_id}")]
@@ -16,15 +16,14 @@ pub async fn roll(
     path: web::Path<i32>,
 ) -> Result<HttpResponse, AppErrors> {
     let groupe_id = path.into_inner();
+    let groupe = pool.get_groupe_secured(groupe_id, user.id).await?;
+    if groupe.id != groupe_id {
+        return Err(AppErrors::NotFound("Groupe not found".to_string()));
+    }
     let characters = pool.get_active_character_in_group(groupe_id).await?;
     if characters.is_empty() {
         return Err(AppErrors::NotFound(
             "Groups doesn't have any active characters".to_string(),
-        ));
-    }
-    if characters[0].user_id != user.id {
-        return Err(AppErrors::NotFound(
-            "You are not the owner of this group".to_string(),
         ));
     }
 
